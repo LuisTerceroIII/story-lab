9bd1d57b925a86d045f4b401e805a912cf3f8da9
Luis Espinoza <luis.espinoza.nav@live.com>
2023-06-05 01:13:33 -0300

    add firebase

--- App.tsx
+++ App.tsx
@@ -2,14 +2,41 @@ import 'react-native-url-polyfill/auto';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { Routes } from './src/routes/routes';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect } from 'react';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-        <StatusBar style="dark" />
-        <Routes />
-    </NavigationContainer>
-  )
+
+    SplashScreen.preventAutoHideAsync();
+
+    const [fontsLoaded, error] = useFonts({
+        'FireSans-Black': require('./assets/fonts/FiraSans-Black.ttf'),
+        'FiraSans-Bold': require('./assets/fonts/FiraSans-Bold.ttf'),
+        'FiraSans-ExtraBold': require('./assets/fonts/FiraSans-ExtraBold.ttf'),
+        'FiraSans-Medium': require('./assets/fonts/FiraSans-Medium.ttf'),
+        'FiraSans-Regular': require('./assets/fonts/FiraSans-Regular.ttf'),
+        'FiraSans-SemiBold': require('./assets/fonts/FiraSans-SemiBold.ttf'),
+    });
+    useEffect(() => {
+        const checkLoadedFont = async () => {
+            if (fontsLoaded) {
+                console.log('fonts loaded')
+                await SplashScreen.hideAsync();
+            }
+            if(error) {
+                console.log('error loading fonts')
+            }
+        }
+        checkLoadedFont()
+    }, [fontsLoaded])
+    
+    return (
+        <NavigationContainer>
+            <StatusBar style="dark" />
+            <Routes />
+        </NavigationContainer>
+    )
 }
 
 
